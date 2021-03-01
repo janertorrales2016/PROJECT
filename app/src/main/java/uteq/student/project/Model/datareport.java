e39030e4f8785fdcb8d2bf59ff30cfa27522ffc3
janertorrales2016 <janer.torrales2016@uteq.edu.ec>
2021-02-28 23:07:45 -0500

    reporte

---
+++ app/src/main/java/uteq/student/project/Model/datareport.java
@@ -0,0 +1,37 @@
+package uteq.student.project.Model;
+
+public class datareport {
+    private String estado, fecha,hora,mensaje;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
--- app/src/main/java/uteq/student/project/Report.java
+++ app/src/main/java/uteq/student/project/Report.java
@@ -1,59 +1,266 @@
 package uteq.student.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.html.WebColors;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import uteq.student.project.Model.datareport;
 import uteq.student.project.R;
 
 public class Report extends AppCompatActivity {
 
-    BarChart graficBarras;
+    //firebase auth to check if user is authenticated.
+    private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
+    private DatabaseReference payRef;
+
+    //creating a list of objects constants
+    List<datareport> paymentUsersList;
+    //List all permission required
+    public static String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    public static int PERMISSION_ALL = 12;
+
+
+    public static File pFile;
+    private File payfile;
+    private PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-        graficBarras = (BarChart) findViewById(R.id.graficabarras);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        payRef =  FirebaseDatabase.getInstance().getReference("datos").child("recordatorios").child("5E:CF:7F:78:64:97");
+
+        pdfView = findViewById(R.id.payment_pdf_viewer);
+
+
+        Button reportButton = findViewById(R.id.button_disable_report);
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                previewDisabledUsersReport();
+
+            }
+        });
+
+        //
+        paymentUsersList = new ArrayList<>();
+
+        //create files in charity care folder
+        payfile = new File("/storage/emulated/0/Report/");
+
+        //check if they exist, if not create them(directory)
+        if ( !payfile.exists()) {
+            payfile.mkdirs();
+        }
+        pFile = new File(payfile, "PaymentUsers.pdf");
+
+        //fetch payment and disabled users details;
+        fetchPaymentUsers();
+    }
+    private void fetchPaymentUsers()
+    {
+
+        payRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+//creating an object and setting to displlay
+                    datareport pays = new datareport();
+                    pays.setEstado(snapshot.child("estado").getValue().toString());
+                    pays.setFecha(snapshot.child("fecha").getValue().toString());
+                    pays.setHora(snapshot.child("hora").getValue().toString());
+                    pays.setMensaje(snapshot.child("mensaje").getValue().toString());
+                    paymentUsersList.add(pays);
+
+
+                }
+                //create a pdf file and catch exception beacause file may not be created
+                try {
+                    createPaymentReport(paymentUsersList);
+                } catch (DocumentException | FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
-    public void btnGenerate(View view){
-        //entrada con los valores de entrada
-        List<BarEntry> entradas = new ArrayList<>();
-
-        entradas.add( new BarEntry(0,2));
-        entradas.add( new BarEntry(1,2));
-        entradas.add( new BarEntry(2,2));
-        entradas.add( new BarEntry(3,2));
-        entradas.add( new BarEntry(4,8));
-        entradas.add( new BarEntry(5,2));
-
-        //mandamos los datos para crear la grafica
-        BarDataSet datos = new BarDataSet(entradas, "Reporte de recordatorios");
-        BarData data = new  BarData(datos);
-
-        //ponemos el color a cada barra
-        datos.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        //separacion etre las barras
-        data.setBarWidth(0.9f);
-
-        graficBarras.setData(data);
-
-        //poner las barras centrada
-        graficBarras.setFitBars(true);
-
-        graficBarras.invalidate(); //hacer refresh
+    private void createPaymentReport(  List<datareport> paymentUsersList) throws DocumentException, FileNotFoundException{
+        BaseColor colorWhite = WebColors.getRGBColor("#ffffff");
+        BaseColor colorBlue = WebColors.getRGBColor("#056FAA");
+        BaseColor grayColor = WebColors.getRGBColor("#425066");
 
 
+
+        Font white = new Font(Font.FontFamily.HELVETICA, 15.0f, Font.BOLD, colorWhite);
+        FileOutputStream output = new FileOutputStream(pFile);
+        Document document = new Document(PageSize.A4);
+        PdfPTable table = new PdfPTable(new float[]{6, 25, 20, 20});
+        table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.getDefaultCell().setFixedHeight(50);
+        table.setTotalWidth(PageSize.A4.getWidth());
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        Chunk noText = new Chunk("No.", white);
+        PdfPCell noCell = new PdfPCell(new Phrase(noText));
+        noCell.setFixedHeight(50);
+        noCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        noCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+        Chunk nameText = new Chunk("First Name", white);
+        PdfPCell nameCell = new PdfPCell(new Phrase(nameText));
+        nameCell.setFixedHeight(50);
+        nameCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        nameCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+        Chunk phoneText = new Chunk("Other Names", white);
+        PdfPCell phoneCell = new PdfPCell(new Phrase(phoneText));
+        phoneCell.setFixedHeight(50);
+        phoneCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        phoneCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+        Chunk amountText = new Chunk("Phone Number", white);
+        PdfPCell amountCell = new PdfPCell(new Phrase(amountText));
+        amountCell.setFixedHeight(50);
+        amountCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        amountCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+
+        Chunk footerText = new Chunk("Moses Njoroge - Copyright @ 2020");
+        PdfPCell footCell = new PdfPCell(new Phrase(footerText));
+        footCell.setFixedHeight(70);
+        footCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        footCell.setVerticalAlignment(Element.ALIGN_CENTER);
+        footCell.setColspan(4);
+
+
+        table.addCell(noCell);
+        table.addCell(nameCell);
+        table.addCell(phoneCell);
+        table.addCell(amountCell);
+        table.setHeaderRows(1);
+
+        PdfPCell[] cells = table.getRow(0).getCells();
+
+
+        for (PdfPCell cell : cells) {
+            cell.setBackgroundColor(grayColor);
+        }
+        for (int i = 0; i < paymentUsersList.size(); i++) {
+            datareport pay = paymentUsersList.get(i);
+
+            String id = pay.getFecha();
+            String name = pay.getHora();
+            String sname = pay.getMensaje();
+            String phone = pay.getEstado();
+
+
+            table.addCell(id);
+            table.addCell(name);
+            table.addCell(sname);
+            table.addCell(phone);
+
+        }
+
+        PdfPTable footTable = new PdfPTable(new float[]{6, 25, 20, 20});
+        footTable.setTotalWidth(PageSize.A4.getWidth());
+        footTable.setWidthPercentage(100);
+        footTable.addCell(footCell);
+
+        PdfWriter.getInstance(document, output);
+        document.open();
+        Font g = new Font(Font.FontFamily.HELVETICA, 25.0f, Font.NORMAL, grayColor);
+        document.add(new Paragraph(" How to generate real-time reports using Firebase\n\n", g));
+        document.add(table);
+        document.add(footTable);
+
+        document.close();
+    }
+
+    public boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+    public void previewDisabledUsersReport()
+    {
+        if (hasPermissions(this, PERMISSIONS)) {
+            DisplayReport();
+        } else {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+    private void DisplayReport()
+    {
+        pdfView.fromFile(pFile)
+                .pages(0,2,1,3,3,3)
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .load();
     }
 }
