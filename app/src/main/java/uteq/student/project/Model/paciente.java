8dbe4f8093296538d913ed4f0a25e1323f3bf937
janertorrales2016 <janer.torrales2016@uteq.edu.ec>
2021-03-01 02:01:08 -0500

    .

--- app/src/main/java/uteq/student/project/Model/paciente.java
+++ app/src/main/java/uteq/student/project/Model/paciente.java
@@ -32,14 +32,6 @@ public class paciente {
     }
 
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     @Override
     public String toString() {
         return nombre;
